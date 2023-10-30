f1b4b47ac44f79be2d5b0d99c7b8bcb1e37408f2
arkadeep22 <101667807+arkadeep22@users.noreply.github.com>
2023-10-30 23:54:59 +0530

    first commit

--- controller/user.js
+++ controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
   
       const activationToken = createActivationToken(user);
   
-      const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+      const activationUrl = `https://arkaopss.onrender.com/activation/${activationToken}`;
   
       try {
         await sendMail({
